db3240759fc4f8c3c91a3d9c10f02d0916aac3c1
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-12-05 13:14:52 +0100

    Created the list of members in js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const teamMember = [
+    {
+        fullname: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        fullname: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        fullname: 'Walter Gordon',
+        role: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg',
+    },
+
+    {
+        fullname: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        fullname:'Scott Estrada',
+        role:'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+
+    {
+        fullname:'Barbara Ramos',
+        role:'Graphic Designer',
+        img:'barbara-ramos-graphic-designer.jpg'
+    },
+]
