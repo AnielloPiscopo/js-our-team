efedffec0a2afa20a1057891f360635f2e29b50c
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-12-05 16:36:51 +0100

    Added some improvements and functions

--- js/script.js
+++ js/script.js
@@ -49,16 +49,69 @@ const teamMembers = [
 CODICE PRINCIPALE*/
 
 // * CODICE LINEARE
+
+// ? PRELIEVO DAL DOM DELLA SEZIONE DELLE CARTE
+const teamMemberCardsSection = document.querySelector('main .my-cards');
+
+
+// ? VISUALIZZAZIONE IN CONSOLE DELL'ARRAY DI PARTENZA
 console.log(teamMembers);
 
 
+// ? CICLO FOR
 for(let i=0 ; i< teamMembers.length ; i++){
+    // * Console log del singolo membro del team
     const teamMember = teamMembers[i];
-    
     console.log(teamMember);
+
+
+    // * Creazione delle singole carte
+    const teamMemberCard = putAnElementWithClassesInHtml('article' , 'my-card' , teamMemberCardsSection)
 
 
     for(let key in teamMember){
         console.log(teamMember[key]);
     }
+}
+
+
+
+
+
+/* ------------------------------------------------------------------------
+FUNZIONI */
+
+// * FUNZIONE PER CREARE UN ELEMENTO HTML SENZA CLASSI
+function getAnElement(element){
+    let htmlElement = document.createElement(element);
+    return htmlElement;
+}
+
+
+
+// * FUNZIONE PER CREARE UN ELEMENTO HTML CON UNA O PIU' CLASSI
+function getAnElementWithClasses(element , elementClasses){
+    let htmlElement = getAnElement(element);
+
+    htmlElement.className = elementClasses;
+
+    return htmlElement;
+}
+
+
+
+// * FUNZIONE PER INSERIRE UN ELEMENTO NELL'HTML
+function putAnElementInHtml(element , parent){
+    let child = getAnElement(element);
+
+    parent.append(child);
+}
+
+
+
+// * FUNZIONE PER INSERIRE UN ELEMENTO CON CLASSI NELL'HTML
+function putAnElementWithClassesInHtml(element , elementClasses, parent , action){
+    let child = getAnElementWithClasses(element , elementClasses);
+
+    parent.append(child);
 }
